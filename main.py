c7f7f7cddd0f31e2d4458843708a66ac80daf266
Utsab Adhikari <100577045+YOURNAME116@users.noreply.github.com>
2023-08-09 04:59:20 +0545

    main.py

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ import yara
 import time
 import sys
 import hashlib
-# import pyautogui
-import signal
 import send2trash
 import re
 
-
+logo = ''' _\n\n\n___    ____  ___      .______          ___              _______.  ______     ___      .__   __. .__   __.  _______ .______      
+\   \  /   / /   \     |   _  \        /   \            /       | /      |   /   \     |  \ |  | |  \ |  | |   ____||   _  \     
+ \   \/   / /  ^  \    |  |_)  |      /  ^  \          |   (----`|  ,----'  /  ^  \    |   \|  | |   \|  | |  |__   |  |_)  |    
+  \_    _/ /  /_\  \   |      /      /  /_\  \          \   \    |  |      /  /_\  \   |  . `  | |  . `  | |   __|  |      /     
+    |  |  /  _____  \  |  |\  \----./  _____  \     .----)   |   |  `----./  _____  \  |  |\   | |  |\   | |  |____ |  |\  \----.
+    |__| /__/     \__\ | _| `._____/__/     \__\    |_______/     \______/__/     \__\ |__| \__| |__| \__| |_______|| _| `._____|
+                                                                                                                                 \n\n'''
 
 def read_md5_hashes_file(file_path):
     """Read MD5 hashes from the given file."""
@@ -40,7 +44,7 @@ def compare_hashes_with_file(directory_path, file_hashes):
             file_path = os.path.join(root, filename)
             print(f"Scanning {file_path} with hash")
             try:
-                md5_hash = get_file_md5(file_path)  # Assuming you have a function get_file_md5 to calculate MD5 hash
+                md5_hash = get_file_md5(file_path) 
                 if md5_hash in file_hashes:
                     print(f"File '{filename}' is a malware detected by hash.")
                     detected_malware.append(file_path)
@@ -49,7 +53,7 @@ def compare_hashes_with_file(directory_path, file_hashes):
                 print(f"Permission error while accessing '{filename}'. Continuing to scan other files.")
     
     return detected_malware
-# Call the compare_hashes_with_file function with the desired directory_path and file_hashes
+
 
   
             
@@ -76,6 +80,7 @@ class YaraScanner:
 
             if matches:
                 output_lines = []
+                output_lines.append(logo)
                 output_lines.append(f"\nYARA rules matched the file: {file_path}\n")
                 for rule_name, rule_match_list in matches.items():
                     for rule_match in rule_match_list:
@@ -162,6 +167,7 @@ class YaraScanner:
                             output_lines.extend(result)
 
                 if not output_lines:
+                    output_lines.append(logo)
                     output_lines.append(f"YARA rules did not match any file in the directory '{directory}'.")
                 return output_lines
             else:
@@ -194,27 +200,41 @@ class YaraScanner:
 
         return None
 
-    # def signal_handler(signal, frame):
-    #     print("\nScan interrupted by user.")
-    #     sys.exit(0)
+def move_files_to_recycle_bin(output):
+    file_paths = re.findall(r"matched the file: (.+)\n", output)
+    for normalized_file_path in file_paths:
+        normalized_file_path = os.path.normpath(normalized_file_path)
+        user_input = input(f"Do you want to move the file '{normalized_file_path}' to the recycle bin? (y/n): ").lower()
+        if user_input == "y":
+            try:
+                send2trash.send2trash(normalized_file_path)  # Move the file to the recycle bin
+                print(f"File '{normalized_file_path}' moved to the recycle bin.")
+            except Exception as e:
+                print(f"Error moving file '{normalized_file_path}' to the recycle bin: {e}")
+        elif user_input == "n":
+            print(f"File '{normalized_file_path}' was not moved to the recycle bin.")
+        else:
+            print("Invalid input. File was not moved to the recycle bin.")
 def main():
     parser = argparse.ArgumentParser(description="YARA Scanner")
     parser.add_argument("-f", "--file", help="File path to scan")
     parser.add_argument("-d", "--directory", help="Directory path to scan")
     parser.add_argument("-r", "--recursive", action="store_true", help="Enable recursive scanning")
-    parser.add_argument("-H","--hash",action="store_true",help="scan wish hash")
+    parser.add_argument("-H","--hash",action="store_true",help="scan with hash")
     parser.add_argument("-t", "--threads", type=int, default=2, help="Number of threads (default: 2)")
     parser.add_argument("-o", "--output", help="Output file path")
     args = parser.parse_args()
     dir=args.directory
     if args.hash:
-        hashes_file_path = "C:\\Users\\Acer\\OneDrive - Softwarica College\\Desktop\\course work python\\coursework2\\hash.txt"
+        hashes_file_path = "./hash.txt"
         file_hashes = read_md5_hashes_file(hashes_file_path)
 
     
         compare_hashes_with_file(dir, file_hashes) 
         detected_malware = compare_hashes_with_file(dir, file_hashes)
         if detected_malware:
+
+            print(logo)
             print("\nDetected malware files:")
             for file_path in detected_malware:
                 print(file_path)
@@ -242,7 +262,7 @@ def main():
 
     
     if args.file or args.directory:
-        rule_file_path = "C:\\Users\\Acer\\OneDrive - Softwarica College\\Desktop\\course work python\\malware_rule.yar"
+        rule_file_path = "malware_rule.yar"
         num_threads = args.threads
             # Read MD5 hashes from the file
 
@@ -252,6 +272,7 @@ def main():
 
         if output:
             if args.output:
+                
                 print(output)
                 if args.output.startswith("YARA_") and args.output.endswith(".txt"):
                     with open(args.output, "a") as output_file:
@@ -269,25 +290,16 @@ def main():
                     with open("YARA_" + args.output + ".txt", "a") as output_file:
                         output_file.write(output)
                     print(f"File saved to YARA_{args.output}.txt")
+                    
+                move_files_to_recycle_bin(output)
+
               
                    
 
             else:
                 print(output)
-                file_paths = re.findall(r"matched the file: (.+)\n", output)
-                for normalized_file_pather in file_paths:
-                    normalized_file_pather = os.path.normpath(normalized_file_pather)
-                    user_input = input(f"Do you want to move the file '{normalized_file_pather}' to the recycle bin? (y/n): ").lower()
-                    if user_input == "y":
-                            try:
-                                send2trash.send2trash(normalized_file_pather)  # Move the file to the recycle bin
-                                print(f"File '{normalized_file_pather}' moved to the recycle bin.")
-                            except Exception as e:
-                                print(f"Error moving file '{normalized_file_pather}' to the recycle bin: {e}")
-                    elif user_input == "n":
-                            print(f"File '{normalized_file_pather}' was not moved to the recycle bin.")
-                    else:
-                            print("Invalid input. File was not moved to the recycle bin.")
+                move_files_to_recycle_bin(output)
+            
         elif output== "directory_not_found":
             print(output)
         else:
